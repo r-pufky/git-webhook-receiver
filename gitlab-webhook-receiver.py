35f5ee8ec8c5d84cae9c364b131979c1ab906242
Robert Pufky <rpufky@gmail.com>
2019-03-07 15:56:29 -0800

    Move config constants into relevant config/header objects.

--- gitlab-webhook-receiver.py
+++ gitlab-webhook-receiver.py
@@ -21,24 +21,24 @@ logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                     level=logging.DEBUG,
                     stream=sys.stdout)
 
-HEADER_CONTENT_LENGTH = 'Content-Length'
-HEADER_TOKEN = 'X-Gitlab-Token'
-CONFIG_COMMAND = 'command'
-CONFIG_TOKEN = 'gitlab_token'
-CONFIG_BACKGROUND = 'background'
-JSON_PROJECT = 'project'
-JSON_PROJECT_NAME = 'name'
-
 
 class ReceiverHeader(object):
   """ Encapsulate processed received headers.
 
   Attributes:
+    CONTENT_LENGTH: String header for content length.
+    TOKEN: String header for token.
+    PROJECT: String key for project in JSON headers.
+    PROJECT_NAME: String key for project name in JSON headers.
     token: String request token.
     payload: Filehandle like object containing all JSON header content.
     project: String project name for update.
     params: Dictionary of parsed JSON header content.
   """
+  CONTENT_LENGTH = 'Content-Length'
+  TOKEN = 'X-Gitlab-Token'
+  PROJECT = 'project'
+  PROJECT_NAME = 'name'
 
   def __init__(self):
     """ Initialize recieved headers. """
@@ -52,10 +52,16 @@ class ProjectConfig(object):
   """ Composed config object.
 
   Attributes:
+    COMMAND: String key for command in config.
+    TOKEN: String key for token in config.
+    BACKGROUND: String key for background in config.
     command: String command to execute.
     token: String authorized token.
     foreground: Boolean True if command should be executed in the foreground.
   """
+  CONFIG_COMMAND = 'command'
+  CONFIG_TOKEN = 'gitlab_token'
+  CONFIG_BACKGROUND = 'background'
 
   def __init__(self):
     """ Initialize config. """
@@ -77,14 +83,14 @@ class RequestHandler(BaseHTTPRequestHandler):
       KeyError: if project name cannot be parsed from received headers.
     """
     headers = ReceiverHeader()
-    headers.token = self.headers[HEADER_TOKEN]
-    headers.payload = self.rfile.read(int(self.headers[HEADER_CONTENT_LENGTH]))
+    headers.token = self.headers[headers.TOKEN]
+    headers.payload = self.rfile.read(int(self.headers[headers.CONTENT_LENGTH]))
 
     if len(headers.payload) > 0:
       headers.params = json.loads(headers.payload.decode('utf-8'))
 
     try:
-      headers.project = headers.params[JSON_PROJECT][JSON_PROJECT_NAME]
+      headers.project = headers.params[headers.PROJECT][headers.PROJECT_NAME]
     except KeyError as err:
       logging.error('No project provided by the JSON payload.')
       raise
@@ -103,34 +109,34 @@ class RequestHandler(BaseHTTPRequestHandler):
     Raises:
       KeyError: If the requested key is not found in the config file.
     """
-    project_config = ProjectConfig()
+    pconfig = ProjectConfig()
     try:
-      project_config.command = config[project][CONFIG_COMMAND]
-      project_config.token = config[project][CONFIG_TOKEN]
-      project_config.foreground = (CONFIG_BACKGROUND in config[project] and
-                                   not config[project][CONFIG_BACKGROUND])
+      pconfig.command = config[project][pconfig.COMMAND]
+      pconfig.token = config[project][pconfig.TOKEN]
+      pconfig.foreground = (pconfig.BACKGROUND in config[project] and
+                            not config[project][pconfig.BACKGROUND])
     except KeyError as err:
       if err == project:
         logging.error('Project %s not found in %s.', project, args.cfg.name)
-      elif err == CONFIG_COMMAND:
-        logging.error('Key %s not found in %s.', CONFIG_COMMAND, args.cfg.name)
-      elif err == CONFIG_TOKEN:
-        logging.error('Key %s not found in %s.', CONFIG_TOKEN, args.cfg.name)
+      elif err == pconfig.COMMAND:
+        logging.error('Key %s not found in %s.', pconfig.COMMAND, args.cfg.name)
+      elif err == pconfig.TOKEN:
+        logging.error('Key %s not found in %s.', pconfig.TOKEN, args.cfg.name)
 
     logging.info('Loaded project %s and run command %s.',
                  project,
-                 project_config.command)
-    return project_config
+                 pconfig.command)
+    return pconfig
 
   def ProcessRequest(self, headers):
     """ Process request and execute command. """
-    project_config = self.ParseConfig(headers.project)
+    pconfig = self.ParseConfig(headers.project)
 
-    if headers.token == project_config.token:
-      logging.info('Start executing %s' % project_config.command)
+    if headers.token == pconfig.token:
+      logging.info('Start executing %s' % pconfig.command)
       try:
-        p = Popen(project_config.command, stdin=PIPE)
-        if project_config.foreground:
+        p = Popen(pconfig.command, stdin=PIPE)
+        if pconfig.foreground:
           p.communicate()
         self.send_response(200, 'OK')
       except OSError as err:
